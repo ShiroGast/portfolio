9ae94f610b02f207b211c0bff915b62522988340
Shiro <63391697+ShiroGast@users.noreply.github.com>
2024-09-05 10:19:08 +0200

    Basic css for nav and main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,42 +4,51 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Portfolio</title>
 </head>
 <body>
-    <head>
-        <nav>
-            <div class="logo">
-                <img src="logo.png" alt="Logo">
-            </div>
-            <div>
-                <a href="">Login</a>
-            </div>
-        </nav>
+    <head id="home">
+        <div class="header">
+            <nav>
+                <div class="logo">
+                    <img src="logo.png" alt="Logo">
+                </div>
+                <div class="links">
+                    <a href="http://portfolio.test/#home">Home</a>
+                    <a href="http://portfolio.test/#about">About</a>
+                    <a href="http://portfolio.test/#projects">Projects</a>
+                    <a href="http://portfolio.test/#contact">Contact</a>
+                </div>
+                <div class="login">
+                    <a href="">Login</a>
+                    <a href="">Register</a>
+                </div>
+            </nav>
+        </div>
     </head>
     <main>
-        <section>
-            <div>
+        <section class="portfolio">
+            <div class="portfolio_text">
                 <h1>Portfolio</h1>
                 <p>Welcome to my portfolio website! Here you can explore my work and learn more about me. Feel free to browse through the sections and don't hesitate to reach out if you have any questions or inquiries. Enjoy your visit!</p>
             </div>
-            </div>
         </section>
-        <section>
-            <div>
+        <section id="about">
+            <div class="about_text">
                 <h2>About me</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sit assumenda vitae quis blanditiis temporibus pariatur cum laboriosam corrupti unde officiis dolores odio cumque voluptatem quibusdam, aliquid, id minus iusto? Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus dignissimos ut sit temporibus illum ad totam quasi, eveniet ex debitis esse harum quia accusantium eaque tempore earum corporis expedita deleniti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto amet, maiores laborum, obcaecati aperiam quis sed magnam praesentium corrupti hic temporibus aliquam assumenda est laudantium fugiat tempore accusamus ex! Id! Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum minima ratione velit ipsum illo at voluptates, aspernatur consequatur adipisci ut, officia vitae ex accusamus sit numquam eligendi cumque, necessitatibus veritatis?</p>
             </div>
-            <div>
+            <div class="about_img">
                 <img src="me.png" alt="Img placeholder">
             </div>
         </section>
-        <section>
-            <div>
+        <section id="projects">
+            <div class="projects_text">
                 <h2>Projects</h2>
                 <p>These are some of my projects:</p>
             </div>
-            <div>
+            <div class="projects">
                 <div>
                     <img src="project1.png" alt="Project 1 img">
                     <p>Project 1</p>
@@ -54,13 +63,13 @@
                 </div>
             </div>
         </section>
-        <section>
-            <div>
+        <section id="contact">
+            <div class="contact_text">
                 <h2>Contact</h2>
                 <p>You can contact me at: <a href="mailto:shiorgast@gmail.com">shirogast@gmail.com</a></p>
                 <p>Or you can send me a message usig this form:</p>
             </div>
-            <div>
+            <div class="contact_form">
                 <form action="">
                     <input type="text" placeholder="Name">
                     <input type="email" placeholder="Email">
@@ -71,7 +80,6 @@
         </section>
     </main>
     <footer>
-
     </footer>
 </body>
 </html>
